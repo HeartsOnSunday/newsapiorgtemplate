29483ad983399736ffa2f96d1d90f6c3d6b75090
Leah <rhodycodes401@gmail.com>
2017-12-01 02:22:13 +0000

    must fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
   MSG Data Response are all recognized
   APIKEY
 */
+
 $(document).ready(function() {
     $.ajax({
         method: "GET",
@@ -12,19 +13,31 @@ $(document).ready(function() {
             if (data.status == "ok") {
                 console.log(data);
                 for (var i = 0; i < data.sources.length; i++) {
-                   var source =  document.createElement("OPTION");
+                   var source = document.createElement("OPTION");
+                   source.setAttribute("id", "dropdown");
+                   source.setAttribute("value", data.sources[i].id);
                    source.innerHTML = data.sources[i].name;
                    document.getElementById("selection").appendChild(source);
+                   
+                   var WOO = document.createElement("P");
+                   WOO.setAttribute("id", "HOO");
+                   WOO.setAttribute("value", data.sources[i].description);
+                   WOO.innerHTML = data.sources[i].description;
+                   document.getElementById("FIXTHIS").appendChild(WOO);
                 }
-                
-                
-                
                 alert(data.status);
-            } else {
-                alert(data.message);
+                alert(data.sources[0].description);
             }
         }
-    })
+    });
+
+    $("#source").submit( function(event) {
+        event.preventDefault();
+        alert(document.getElementById("selection").value);
+        alert(document.getElementById("HOO").value);
+    });   
+  });
+
 //      success: function(data){
 //            if (data.status == "ok") {
 //               alert(data.status);
@@ -34,4 +47,3 @@ $(document).ready(function() {
     // .done(function( data ) {
     //   console.log( data );
     //     console.log( data.status);
-  });
